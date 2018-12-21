e1ceb506dd78892498b5b9b77e5fef2ab64634f6
al <zhang.yang@coding.net>
2018-12-21 23:56:04 +0800

    Drag handler.

--- src/components/ImageHandler.js
+++ src/components/ImageHandler.js
@@ -3,26 +3,48 @@ import './image-handler.css'
 import React from 'react'
 
 export default class ImageHandler extends React.Component {
+  topRef = null
+  containerRef = null
+
+  state = {
+    topPos: 100,
+    botPos: 1,
+  }
+
   componentDidMount() {
-    console.log(this.props.imageIdx, 'ready')
   }
 
   handleDragStart = ev => {
-    console.log('drag start')
   }
 
-  handleDragStop = ev => {
-    console.log('end')
+  handleDrop = (elName, varName) => ev => {
+    const { offsetTop, offsetHeight } = this.containerRef
+    const barPosition = ev.pageY
+    let top = barPosition - offsetTop
+    console.log('drag end: ', top)
+    if (top < 0) {
+      top = 0
+    }
+    this.setState({
+      [varName]: offsetHeight - top
+    })
+  }
+
+  getRef = refName => el => {
+    this[refName] = el
   }
 
   render() {
     const { imageSrc } = this.props
+    const { topPos, botPos } = this.state
+
     return (
       <div
         className="image-handler-container"
         style={{
           position: 'relative',
         }}
+        ref={this.getRef('containerRef')}
       >
         <img
           alt=""
@@ -30,11 +52,42 @@ export default class ImageHandler extends React.Component {
         />
 
         <div
-          className={'select-handler top'}
-          onMouseDown={this.handleDragStart}
-          onMouseMove={this.handleDragStop}
+          className="overlay-indicator"
+          style={{
+            top: 0,
+            height: `calc(100% - ${topPos}px)`,
+          }}
         />
-        <div className={'select-handler bottom'}/>
+
+        <div
+          className={'select-handler top'}
+          style={{
+            bottom: `${topPos}px`
+          }}
+          ref={this.getRef('topRef')}
+          draggable={true}
+          onDrag={this.handleDragStart}
+          onDragEnd={this.handleDrop('topRef', 'topPos')}
+        />
+        <div
+          className={'select-handler bottom'}
+          style={{
+            bottom: `${botPos}px`
+          }}
+          ref={this.getRef('botRef')}
+          draggable={true}
+          onDrag={this.handleDragStart}
+          onDragEnd={this.handleDrop('botRef', 'botPos')}
+        />
+
+        <div
+          className="overlay-indicator"
+          style={{
+            bottom: 0,
+            height: `${botPos}px`,
+          }}
+        />
+
       </div>
     )
   }
